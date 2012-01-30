b10f88af066cc6c85c63914ee2dd3e2bd25c8400
Carey Bishop <carey.bishop+github@gmail.com>
2012-01-31 00:43:59 +1300

    Added USB device list window

---
+++ Second-Law/UsbDeviceList.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SecondLaw {
+	partial class UsbDeviceList {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.lsvDevices = new System.Windows.Forms.ListView();
+			this.imlSmall = new System.Windows.Forms.ImageList(this.components);
+			this.SuspendLayout();
+			// 
+			// lsvDevices
+			// 
+			this.lsvDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lsvDevices.FullRowSelect = true;
+			this.lsvDevices.Location = new System.Drawing.Point(0, 0);
+			this.lsvDevices.Name = "lsvDevices";
+			this.lsvDevices.Size = new System.Drawing.Size(282, 255);
+			this.lsvDevices.SmallImageList = this.imlSmall;
+			this.lsvDevices.TabIndex = 6;
+			this.lsvDevices.UseCompatibleStateImageBehavior = false;
+			this.lsvDevices.View = System.Windows.Forms.View.Details;
+			// 
+			// imlSmall
+			// 
+			this.imlSmall.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+			this.imlSmall.ImageSize = new System.Drawing.Size(16, 16);
+			this.imlSmall.TransparentColor = System.Drawing.Color.Transparent;
+			// 
+			// UsbDeviceList
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(282, 255);
+			this.Controls.Add(this.lsvDevices);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Name = "UsbDeviceList";
+			this.Text = "USB Devices";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UsbDeviceList_FormClosed);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lsvDevices;
+		private System.Windows.Forms.ImageList imlSmall;
+	}
+}
---
+++ Second-Law/UsbDeviceList.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SecondLaw {
+	public partial class UsbDeviceList : Form {
+		private Hardware _hardware;
+
+		public UsbDeviceList(Hardware hardware) {
+			InitializeComponent();
+
+			_hardware = hardware;
+			_hardware.DeviceInterfaceChanged += Hardware_DeviceInterfaceChanged;
+
+			RefreshList();
+		}
+
+		private void Hardware_DeviceInterfaceChanged(object sender, DeviceInterfaceChangedArgs e) {
+			RefreshList();
+		}
+
+		private void RefreshList() {
+			var devices = _hardware.EnumerateUsbDevices();
+			lsvDevices.Items.Clear();
+			lsvDevices.Columns.Clear();
+			lsvDevices.Columns.Add("Name", 250);
+			lsvDevices.Columns.Add("Manufacturer", 250);
+			lsvDevices.Columns.Add("Configuration", 100);
+			lsvDevices.Columns.Add("Vendor ID", 100);
+			lsvDevices.Columns.Add("Product ID", 100);
+			lsvDevices.Columns.Add("Revision", 100);
+			lsvDevices.Columns.Add("Interface ID", 100);
+			lsvDevices.Columns.Add("Location Information", 250);
+			lsvDevices.Columns.Add("Physical Device Object Name", 500);
+			lsvDevices.Columns.Add("Hardware IDs", 500);
+
+			foreach (var usbDevice in devices) {
+				var deviceItem = new ListViewItem(new[] {
+				  usbDevice.DeviceDescription,
+					usbDevice.Manufacturer,
+					usbDevice.Configuration.ToString(),
+					usbDevice.VendorId.ToString("X4"),
+					usbDevice.ProductId.ToString("X4"),
+					usbDevice.Revision.ToString("X4"),
+					(usbDevice.InterfaceId == 0xFFFF) ? "" : usbDevice.InterfaceId.ToString("X2"),
+					usbDevice.LocationInformation,
+					usbDevice.PhysicalDeviceObjectName,
+					string.Join(", ", usbDevice.HardwareIds)
+				});
+
+				// Load the device icons
+				string imageKey = usbDevice.PhysicalDeviceObjectName;
+				//LoadImage(imageKey, usbDevice.LargeIcon, imlLarge);
+				LoadImage(imageKey, usbDevice.SmallIcon, imlSmall);
+				deviceItem.ImageKey = imageKey;
+
+				lsvDevices.Items.Add(deviceItem);
+			}
+		}
+
+		private static void LoadImage(string key, Bitmap image, ImageList imageList) {
+			if (!imageList.Images.ContainsKey(key) && (image != null)) {
+				imageList.Images.Add(key, image);
+			}
+		}
+
+		private void UsbDeviceList_FormClosed(object sender, FormClosedEventArgs e) {
+			_hardware.DeviceInterfaceChanged -= Hardware_DeviceInterfaceChanged;
+			_hardware = null;
+		}
+	}
+}
