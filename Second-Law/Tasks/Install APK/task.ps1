99c748f5fc3b8e5820e340e8ee2ea5bb3534dbf1
Carey Bishop <carey.bishop+github@gmail.com>
2012-06-23 22:39:07 +1200

    Install APK task now works
    task.ps1 now calls InstallPackage on the supplied DeviceInstance to
    actually install the package.

--- Second-Law/Tasks/Install APK/task.ps1
+++ Second-Law/Tasks/Install APK/task.ps1
@@ -1,13 +1,15 @@
-[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 
 Function Get-FileName()
 {
-    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.filter = "Android Package (*.apk)| *.apk"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $OpenFileDialog.filename
+	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+	$OpenFileDialog.title = "Select package to install on " + $Device.Metadata.ProductName
+	$OpenFileDialog.filter = "Android Package (*.apk)| *.apk"
+	$OpenFileDialog.ShowDialog() | Out-Null
+	$OpenFileDialog.filename
 }
 
 # *** Entry Point to Script ***
 
-Get-FileName
+$FileName = Get-FileName;
+$Device.InstallPackage($FileName)
