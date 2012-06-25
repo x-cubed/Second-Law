162ef647e46ad2e40bf934483a02bbbf6c254ff3
Carey Bishop <carey.bishop+github@gmail.com>
2012-06-26 10:59:19 +1200

    Add zergRush exploit script
    Sourced from http://forum.xda-developers.com/showthread.php?t=1296916

---
+++ Second-Law/Tasks/Root with zergrush/task.ps1
@@ -0,0 +1,18 @@
+﻿# ZergRush exploit for temporary root access
+# Works with most Android Froyo (2.2)/Gingerbread (2.3) devices
+#
+# Exploit developed by Revolutionary Team
+# For more information see: http://forum.xda-developers.com/showthread.php?t=1296916
+# To donate visit: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NQRTG2YL4FPXY
+
+# Create a new directory
+$Device.RunADBCommandOrThrow("shell cd /data/local && mkdir tmp")
+$Device.RunADBCommandOrThrow("shell cd /data/local/tmp/ && rm *")
+
+# Push the exploit
+$Device.PushFile("zergRush", "/data/local/tmp")
+$Device.ChangeMode(777, "/data/local/tmp/zergRush");
+
+# Run the exploit
+$Device.RunADBCommandOrThrow("shell ./data/local/tmp/zergRush")
+$Device.WaitForDevice()
