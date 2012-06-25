31d9d0e8e412474873ff68ba5676629bc539fecf
Carey Bishop <carey.bishop+github@gmail.com>
2012-06-26 11:28:23 +1200

    Write progress of exploit

--- Second-Law/Tasks/Root with zergrush/task.ps1
+++ Second-Law/Tasks/Root with zergrush/task.ps1
@@ -6,13 +6,21 @@
 # To donate visit: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NQRTG2YL4FPXY
 
 # Create a new directory
+Write-Progress -PercentComplete 0 -Status "Creating temporary folder..."
 $Device.RunADBCommandOrThrow("shell cd /data/local && mkdir tmp")
 $Device.RunADBCommandOrThrow("shell cd /data/local/tmp/ && rm *")
 
 # Push the exploit
+Write-Progress -PercentComplete 10 -Status "Uploading exploit..."
 $Device.PushFile("zergRush", "/data/local/tmp")
 $Device.ChangeMode(777, "/data/local/tmp/zergRush");
 
 # Run the exploit
+Write-Progress -PercentComplete 20 -Status "Running exploit..."
 $Device.RunADBCommandOrThrow("shell ./data/local/tmp/zergRush")
+
+# Wait for the device to reconnect with ADB as root
+Write-Progress -PercentComplete 90 -Status "Waiting for device to reconnect..."
 $Device.WaitForDevice()
+
+Write-Progress -Completed
