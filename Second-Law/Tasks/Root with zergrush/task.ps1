b2fa5bf9df67b7b7b7041a74c351cb1293e6abf5
Carey Bishop <carey.bishop+github@gmail.com>
2012-06-26 16:38:05 +1200

    Use variables in script

--- Second-Law/Tasks/Root with zergrush/task.ps1
+++ Second-Law/Tasks/Root with zergrush/task.ps1
@@ -5,22 +5,30 @@
 # For more information see: http://forum.xda-developers.com/showthread.php?t=1296916
 # To donate visit: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NQRTG2YL4FPXY
 
+$TempFolder = "/data/local/tmp/";
+$Executable = $TempFolder + "/zergRush";
+
 # Create a new directory
 Write-Progress -Activity "Getting root access..." -PercentComplete 0 -Status "Creating temporary folder..."
 $Device.RunShellCommand("cd /data/local && mkdir tmp")
-$Device.RunShellCommand("cd /data/local/tmp/ && rm *")
+$Device.RunShellCommand("cd " + $TempFolder + " && rm *")
 
 # Push the exploit
 Write-Progress -Activity "Getting root access..." -PercentComplete 10 -Status "Uploading exploit..."
-$Device.PushFile("zergRush", "/data/local/tmp")
-$Device.ChangeMode(777, "/data/local/tmp/zergRush");
+$Device.PushFile("zergRush", $TempFolder)
+
+# Make it executable
+Write-Progress -Activity "Getting root access..." -PercentComplete 20 -Status "Preparing exploit..."
+$Device.ChangeMode(777, $Executable);
 
 # Run the exploit
-Write-Progress -Activity "Getting root access..." -PercentComplete 20 -Status "Running exploit..."
-$Device.RunShellCommand("./data/local/tmp/zergRush")
+Write-Progress -Activity "Getting root access..." -PercentComplete 30 -Status "Running exploit..."
+$Device.RunShellCommand($Executable)
 
 # Wait for the device to reconnect with ADB as root
 Write-Progress -Activity "Getting root access..." -PercentComplete 90 -Status "Waiting for device to reconnect..."
 $Device.WaitForDevice()
 
+# Clean up temp folder
+$Device.RunShellCommand("rmdir " + $TempFolder)
 Write-Progress -Activity "Getting root access..." -Status "Done" -Completed
