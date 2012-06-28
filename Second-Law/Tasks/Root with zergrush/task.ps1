9215c4f91ef5159abc647d2157d43d8111d27ac3
Carey Bishop <carey.bishop+github@gmail.com>
2012-06-28 22:08:50 +1200

    Define the activity name once only

--- Second-Law/Tasks/Root with zergrush/task.ps1
+++ Second-Law/Tasks/Root with zergrush/task.ps1
@@ -5,31 +5,34 @@
 # For more information see: http://forum.xda-developers.com/showthread.php?t=1296916
 # To donate visit: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NQRTG2YL4FPXY
 
+$Activity = "Getting root access..."
+
 $TempFolder = "/data/local/tmp/";
 $Executable = $TempFolder + "/zergRush";
 
 # Create a new directory
-Write-Progress -Activity "Getting root access..." -PercentComplete 0 -Status "Creating temporary folder..."
+Write-Progress -Activity $Activity -PercentComplete 0 -Status "Creating temporary folder..."
 $Device.RunShellCommand("cd /data/local && mkdir tmp")
 $Device.RunShellCommand("cd " + $TempFolder + " && rm *")
 
 # Push the exploit
-Write-Progress -Activity "Getting root access..." -PercentComplete 10 -Status "Uploading exploit..."
+Write-Progress -Activity $Activity -PercentComplete 10 -Status "Uploading exploit..."
 $Device.PushFile("zergRush", $TempFolder)
 
 # Make it executable
-Write-Progress -Activity "Getting root access..." -PercentComplete 20 -Status "Preparing exploit..."
-$Device.ChangeMode(777, $Executable);
+Write-Progress -Activity $Activity -PercentComplete 20 -Status "Preparing exploit..."
+$Device.ChangeMode(777, $Executable)
 
 # Run the exploit
-Write-Progress -Activity "Getting root access..." -PercentComplete 30 -Status "Running exploit..."
+Write-Progress -Activity $Activity -PercentComplete 30 -Status "Running exploit..."
 $Device.RunShellCommand($Executable)
 
 # Wait for the device to reconnect with ADB as root
-Write-Progress -Activity "Getting root access..." -PercentComplete 90 -Status "Waiting for device to reconnect..."
+Write-Progress -Activity $Activity -PercentComplete 90 -Status "Waiting for device to reconnect..."
 $Device.WaitForDevice()
 
 # Clean up temp folder
+Write-Progress -Activity $Activity -PercentComplete 95 -Status "Cleaning up..."
 $Device.RunShellCommand("rm " + $TempFolder + "*")
 $Device.RunShellCommand("rmdir " + $TempFolder)
-Write-Progress -Activity "Getting root access..." -Status "Done" -Completed
+Write-Progress -Activity $Activity -Status "Done" -Completed
